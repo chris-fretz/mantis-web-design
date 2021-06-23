23c06ebe6bbd8effd301a6cdd9f05d3e7d9e8c34
Chris Fretz <chrisfretz@gmail.com>
2021-06-23 17:48:56 -0400

    Fixed sidebar functionality, fixed form submission

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@ const navBtn = document.querySelector("#nav-btn");
 const closeBtn = document.querySelector("#close-btn");
 const sidebar = document.querySelector("#sidebar");
 const date = document.querySelector("#date");
+
 // add fixed class to navbar
 window.addEventListener("scroll", function () {
   if (window.pageYOffset > 80) {
@@ -11,12 +12,29 @@ window.addEventListener("scroll", function () {
     navbar.classList.remove("navbar-fixed");
   }
 });
-// show sidebar
+// sidebar functionality
 navBtn.addEventListener("click", function () {
   sidebar.classList.add("show-sidebar");
 });
 closeBtn.addEventListener("click", function () {
   sidebar.classList.remove("show-sidebar");
 });
+
+function toggleMenu() {
+  if (sidebar.classList.contains("show-sidebar")) {
+    sidebar.classList.remove("show-sidebar");
+  } else {
+    sidebar.classList.add("show-sidebar");
+  }
+}
+
+var menuLinks = document.querySelectorAll(".menuLink")
+
+menuLinks.forEach(
+  function (menuLink) {
+    menuLink.addEventListener("click", toggleMenu)
+  }
+)
+
 // set year
 date.innerHTML = new Date().getFullYear();
